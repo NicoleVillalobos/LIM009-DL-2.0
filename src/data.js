98c404008a4c8b255ebe9368359c431c9be3cff8
Nicole Villalobos <aleidavillalobos111@gmail.com>
2019-04-23 10:39:00 -0500

    agregando funcionalidad ascendente y descendente

--- src/data.js
+++ src/data.js
@@ -3,46 +3,82 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const genero = (data ,tipo) => {
+const validateYears=(age)=>{
+  if(age==""){
+   return 0;
+  }
+  else{
+      return (2019-age);
+  }
+}
+
+const sortByAge=(data,tipo)=>{
+
+//[{},{}2,3,5,6]
+  let newData = data.sort((a,b)=>{
+        if(tipo=="desc"){
+         if(validateYears(b.yearOfBirth)<validateYears(a.yearOfBirth)){return -1;}
+        }
+        else {
+          if(validateYears(b.yearOfBirth)>validateYears(a.yearOfBirth)){return -1;}
+
+        }
+  })
+  return newData;
+}
+
+
+
+const validateRole=(role)=>{
+  if(role["hogwartsStudent"]){
+    return "Student";
+  }
+  else {
+    return  "Staff";
+  }
+}
+
+const gender = (data ,tipo) => {
     console.log("Prueba de data",data);
-  let f =data.filter((mage)=>{
+  let filterData =data.filter((mage)=>{
     if(tipo ==mage.gender){
       console.log(mage);
       return mage;
     }
   })
-   
 
-  return f;
+  return filterData;
 };
 
-
-
-window.genero = genero;
-const role= (data ,tipo) => {
-  console.log("Prueba de data",data);
-let f =data.filter((mage)=>{
-  if(tipo ==mage.gender){
-    console.log(mage);
-    return mage;
-  }
+const filterRole=(data,role)=>{ //staff
+ let filterData=data.filter((mage)=>{//[{}]
+    if(role=="student"){
+     if(mage["hogwartsStudent"]== true){
+     return mage;
+          }
+        }
+     else { //staff
+      if(mage["hogwartsStaff"]== true){
+     return mage;
+          }
+        }
 })
- 
+   return filterData;
+}
 
-return f;
-};
-window.genero = genero;
+const validateHouse=(data,tipo)=>{
+    console.log("Prueba de data",data);
+  let filterData =data.filter((mage)=>{//[{}]
+    if(tipo ==mage.house){
+    console.log(house);
+    return mage;
+}
+  })
+    return filterData;
+}
 
-const sortBy= (dataFull ,tipo) => {
-  let newData;
- if(tipo == "1"){
-  
- }
- else {
-
- }
- 
-
-return null;
-};
-window.sortBy = sortBy;
+window.gender = gender;
+window.sortByAge = sortByAge;
+window.validateYears= validateYears;
+window.validateRole=validateRole;
+window.validateHouse=validateHouse;
--- src/main.js
+++ src/main.js
@@ -1,71 +1,103 @@
 /* Manejo del DOM */
-let magesFull;
-let magesFilter;
-const paintMages= document.getElementById("mage-container");
-const paintMage =(json)=>{
-     console.log(json);
-  
-    paintMages.innerHTML="";
-    json.forEach(element => {
+let magesFull = "";
+let magesFilter = "";
+const paintMages = document.getElementById("mage-container");
+const genre = document.getElementById("gender");
+const roles = document.getElementById("role");
+const house = document.getElementById("house");
+const ascendent = document.getElementById("asc");
+const descendent = document.getElementById("desc");
+const paintMage = (json) => {
+    console.log(json);
+    paintMages.innerHTML = "";
+    json.forEach(eachMages => {
         // console.log(element);
-        append(paintMages,element);
+        append(eachMages);
     });
-    
 }
-let append=(element,x)=>{
-   
-element.insertAdjacentHTML("beforeend",`
+
+let validateFlags = (house) => {
+  if (house == "Gryffindor") {
+      return "banderas/escudogryffindor.JPG";
+  } else if (house == "Slytherin") {
+      return "banderas/escudoslytherin.png";
+  } else if (house == "Hufflepuff") {
+      return "banderas/escudohufflepuff.JPG";
+  } else if (house == "Ravenclaw") {
+      return "banderas/escudoravenclaw.JPG";
+  } else {
+      return "banderas/bandera-vacia.JPG";
+  }
+}
+
+let append = (wizards) => {
+paintMages.insertAdjacentHTML("beforeend", `
 <div class="mages">
-                 <img class="img-mages" src="${x.image}"  alt="">
-                 <br>
-                 <h2>${x.name}</h2>
-                 <span>${x.house}</span>
-                  <div class="info-mage">
-                    <img class="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/1024px-Yes_Check_Circle.svg.png" width="40" height="40" alt="">
-                    <span>Especie: <br>${x.species}</span>
-                  </div>
-                  <div class="info-mage">
-                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/1024px-Yes_Check_Circle.svg.png" width="40" height="40" alt="">
-                      <span>Rol: <br>actor</span>
-                    </div>
-                    <div class="info-mage">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/1024px-Yes_Check_Circle.svg.png" width="40" height="40" alt="">
-                        <span>Edad: <br>${(2019-x.yearOfBirth)}</span>
-                      </div>
-                      <div class="info-mage">
-                          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/1024px-Yes_Check_Circle.svg.png" width="40" height="40" alt="">
-                          <span>Actor: <br>${x.actor}</span>
-                        </div>
-            </div>`);
+<div class="logo-mage">
+<img src="${validateFlags(wizards.house)}">
+</div>
+  <img class="img-mages" src="${wizards.image}"> 
+  <h2>${wizards.name}</h2>
+  <span>${wizards.house}</span>
+  <div class="info-mage">
+   <img class="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/1024px-Yes_Check_Circle.svg.png">
+  <span>Especie: <br>${wizards.species}</span>
+  </div>
+  <div class="info-mage">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/1024px-Yes_Check_Circle.svg.png">
+  <span>Rol: <br> ${validateRole(wizards)} </span>
+  </div>
+  <div class="info-mage"> 
+                      
+   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/1024px-Yes_Check_Circle.svg.png">
+  <span>Edad: <br>${validateYears(wizards.yearOfBirth)}</span>
+  </div>
+  <div class="info-mage">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/1024px-Yes_Check_Circle.svg.png">
+  <span>Actor: <br>${wizards.actor}</span>
+  </div>
+  </div>`);
 }
- 
 
-const genere=document.getElementById("gender");
+ascendent.addEventListener("click", () => {
+  let data=sortByAge(magesFull, "asc");
+    paintMage(data);
+})
 
-genere.addEventListener("change",()=>{
- 
+descendent.addEventListener("click", () => {
+  let data=sortByAge(magesFull, "desc");
+    paintMage(data);
 
-   if(genere.value!=""){
-     
-       let genData=genero(magesFull,genere.value);
-         magesFilter=genData;
-       console.log("data filter",genData);
-       paintMage(genData);
-   }
 })
 
 
-const asc=document.getElementById("asc");
-
-asc.addEventListener("click",()=>{
-  sortBy(magesFilter,"1");
+genre.addEventListener("change", () => {
+    if (genre.value) { //"" -> false  , "human" ->true
+        let genData = gender(magesFull, genre.value);
+        magesFilter = genData;
+        console.log("data filter", genData);
+        paintMage(genData);
+    }
 })
-
-fetch("https://raw.githubusercontent.com/NicoleVillalobos/LIM009-DL-2.0/master/src/data/potter.json")
-.then(function(response) {
-  return response.json();
+roles.addEventListener("change", () => {
+    let roleData = filterRole(magesFull, roles.value);
+    magesFilter = roleData;
+    console.log("data filter", roleData);
+    paintMage(roleData);
 })
-.then(function(myJson) {
-    magesFull=myJson;
-  paintMage(myJson);
+house.addEventListener("change", () => {
+
+        let  houseData = validateHouse(magesFull, house.value);
+    
+        // houseData = validateHouse(magesFilter, house.value);
+    
+    magesFilter = houseData;
+    console.log("data filter", houseData);
+    paintMage(houseData);
+})
+fetch("https://raw.githubusercontent.com/NicoleVillalobos/LIM009-DL-2.0/master/src/data/potter.json").then((response) => {
+    return response.json();
+}).then((myJson) => {
+    magesFull = myJson;
+    paintMage(myJson);
 });
